9191a19ced2975238cad20063ee8c79b350a1c19
NguyenTienDat1303 <nguyentiendat130397mobi@gmail.com>
2020-09-29 22:28:21 +0700

    update

---
+++ profiles_api/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """Allow user to edit their own profile"""
+
+    def has_object_permission(self, request, view, object):
+        """Check user is trying to edit their own profile"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return object.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, object):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return object.user_profile.id == request.user.id
+        
